617a97ded968d7e45b1d2df85480d2a088997f09
hyungoo <hyungu0611@gmail.com>
2019-06-11 01:03:28 +0900

    home

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -2,29 +2,51 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movieTitles = [
-  "Matarix",
-  "Full Metal Jacket",
-  "Oldboy",
-  "Star Wars"
-]
 
-const movieImages = [
-  "https://t1.daumcdn.net/movie/e49c2e4eb419a9813228b5ab6bc5b039362236ea",
-  "https://img1.daumcdn.net/thumb/R338x491/?fname=http://t1.daumcdn.net/cfile/205CF91249EC79F00F",
-  "https://upload.wikimedia.org/wikipedia/ko/thumb/4/48/Old_Boy.jpg/250px-Old_Boy.jpg",
-  "https://upload.wikimedia.org/wikipedia/ko/thumb/6/6a/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg/250px-%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg",
+
+const movies = [
+  {
+    title: "Matrix",
+    poster:"https://t1.daumcdn.net/movie/e49c2e4eb419a9813228b5ab6bc5b039362236ea",
+  },
+  {
+    title: "Full Metal Jacket",
+    poster:"https://img1.daumcdn.net/thumb/R338x491/?fname=http://t1.daumcdn.net/cfile/205CF91249EC79F00F",
+  },
+  {
+    title: "Oldboy",
+    poster:"https://upload.wikimedia.org/wikipedia/ko/thumb/4/48/Old_Boy.jpg/250px-Old_Boy.jpg",
+  },
+  {
+    title: "Star Wars",
+    poster:"https://upload.wikimedia.org/wikipedia/ko/thumb/6/6a/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg/250px-%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg",
+  }
 
 ]
 
 class App extends Component {
+
+  //render: componentWillMount() => render() => componentDidMount()
+
+  //update componentWillReceiveProps() => shouldcomponentUpdate() => true => componentWillUpdate() => render() => componentDidUpdate()
+
+  componentWillMount() {
+      // 이게 1번으로 실행되고,
+  }
+ 
+
+  componentDidMount() {
+    // 이게 3번으로 실행 됨.
+  }
+
+
   render() {
+    // 얘가 2번으로 실행된다.
     return (
       <div className="App">
-        <Movie title={movieTitles[0]} poster={movieImages[0]}/>
-        <Movie title={movieTitles[1]} poster={movieImages[1]}/>
-        <Movie title={movieTitles[2]} poster={movieImages[2]}/>
-        <Movie title={movieTitles[3]} poster={movieImages[3]}/>
+        {movies.map((movie, index) => {
+          return <Movie title={movie.title} poster={movie.poster} key={index} />
+        })}
       </div>
     );
   }
--- movie-app/src/Movie.js
+++ movie-app/src/Movie.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import './Movie.css';
+import PropTypes from 'prop-types';
 
 
 class Movie extends Component {
+
+    static propTypes = {
+        title: PropTypes.string.isRequired, // 부모에게서 string으로 받아야하고, 이 정보는 꼭 와야한다고 말해주는 것. proptypes 설치해야함. $yarn add prop-types하고 import.
+        poster: PropTypes.string.isRequired, // 이걸 해주면 정보가 제대로 오는지, 내가 원하는 형식으로 오는 지 확인 가능.
+    }
+
     render() {
         
         return(
