f502851a4021383192b60a740d00e70df8687d18
hyungoo <hyungu0611@gmail.com>
2019-06-11 23:23:11 +0900

    AJAX

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -19,37 +19,9 @@ class App extends Component {
 
     componentDidMount() {
         // 이게 3번으로 실행 됨.
-        setTimeout(() => {
-            this.setState({
-                movies: [
-
-                    {
-                        title: "Matrix",
-                        poster: "https://t1.daumcdn.net/movie/e49c2e4eb419a9813228b5ab6bc5b039362236ea",
-                    },
-                    {
-                        title: "Full Metal Jacket",
-                        poster: "https://img1.daumcdn.net/thumb/R338x491/?fname=http://t1.daumcdn.net/cfile/205CF91249EC79F00F",
-                    },
-                    {
-                        title: "Oldboy",
-                        poster: "https://upload.wikimedia.org/wikipedia/ko/thumb/4/48/Old_Boy.jpg/250px-Old_Boy.jpg",
-                    },
-                    {
-                        title: "Star Wars",
-                        poster: "https://upload.wikimedia.org/wikipedia/ko/thumb/6/6a/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg/250px-%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg",
-                    },
-
-
-                    {
-                        title: "Trainspotting",
-                        poster: "https://images-na.ssl-images-amazon.com/images/I/51GolvcFJTL.jpg"
-                    }
-                ]
-
-
-            })
-        }, 1000)
+        fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
+        .then(res => console.log(res.json()))
+        .catch(err => console.log(err))
     }
 
     _renderMovies = () => {
