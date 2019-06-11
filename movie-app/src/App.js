31dc899730221a654fde3ebd0418abf971801a5a
Yeohyungoo <hyungu0611@gmail.com>
2019-06-11 17:13:57 +0900

    0611

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,55 +1,72 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Movie from './Movie';
 
 
-
-const movies = [
-  {
-    title: "Matrix",
-    poster:"https://t1.daumcdn.net/movie/e49c2e4eb419a9813228b5ab6bc5b039362236ea",
-  },
-  {
-    title: "Full Metal Jacket",
-    poster:"https://img1.daumcdn.net/thumb/R338x491/?fname=http://t1.daumcdn.net/cfile/205CF91249EC79F00F",
-  },
-  {
-    title: "Oldboy",
-    poster:"https://upload.wikimedia.org/wikipedia/ko/thumb/4/48/Old_Boy.jpg/250px-Old_Boy.jpg",
-  },
-  {
-    title: "Star Wars",
-    poster:"https://upload.wikimedia.org/wikipedia/ko/thumb/6/6a/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg/250px-%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg",
-  }
-
-]
-
 class App extends Component {
 
-  //render: componentWillMount() => render() => componentDidMount()
+    state = {
+    }
 
-  //update componentWillReceiveProps() => shouldcomponentUpdate() => true => componentWillUpdate() => render() => componentDidUpdate()
+    //render: componentWillMount() => render() => componentDidMount()
 
-  componentWillMount() {
-      // 이게 1번으로 실행되고,
-  }
- 
+    //update componentWillReceiveProps() => shouldcomponentUpdate() => true => componentWillUpdate() => render() => componentDidUpdate()
 
-  componentDidMount() {
-    // 이게 3번으로 실행 됨.
-  }
+    componentWillMount() {
+        // 이게 1번으로 실행되고,
+    }
 
 
-  render() {
-    // 얘가 2번으로 실행된다.
-    return (
-      <div className="App">
-        {movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index} />
-        })}
-      </div>
+    componentDidMount() {
+        // 이게 3번으로 실행 됨.
+        setTimeout(() => {
+            this.setState({
+                movies: [
+
+                    {
+                        title: "Matrix",
+                        poster: "https://t1.daumcdn.net/movie/e49c2e4eb419a9813228b5ab6bc5b039362236ea",
+                    },
+                    {
+                        title: "Full Metal Jacket",
+                        poster: "https://img1.daumcdn.net/thumb/R338x491/?fname=http://t1.daumcdn.net/cfile/205CF91249EC79F00F",
+                    },
+                    {
+                        title: "Oldboy",
+                        poster: "https://upload.wikimedia.org/wikipedia/ko/thumb/4/48/Old_Boy.jpg/250px-Old_Boy.jpg",
+                    },
+                    {
+                        title: "Star Wars",
+                        poster: "https://upload.wikimedia.org/wikipedia/ko/thumb/6/6a/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg/250px-%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg",
+                    },
+
+
+                    {
+                        title: "Trainspotting",
+                        poster: "https://images-na.ssl-images-amazon.com/images/I/51GolvcFJTL.jpg"
+                    }
+                ]
+
+
+            })
+        }, 1000)
+    }
+
+    _renderMovies = () => {
+        const movies = this.state.movies.map((movie, index) => {
+            return <Movie title = {movie.title} poster = {movie.poster} key = {index} />
+        })
+        return movies
+    }
+
+    render() {
+        // 얘가 2번으로 실행된다.
+        return (
+            <div className = "App" >
+                {this.state.movies ? this._renderMovies() : 'Loading'}
+            </div>
     );
-  }
+    }
 }
 
 export default App;
--- movie-app/src/Movie.js
+++ movie-app/src/Movie.js
@@ -3,30 +3,59 @@ import './Movie.css';
 import PropTypes from 'prop-types';
 
 
-class Movie extends Component {
+// class Movie extends Component {
+//
+//     static propTypes = {
+//         title: PropTypes.string.isRequired, // 부모에게서 string으로 받아야하고, 이 정보는 꼭 와야한다고 말해주는 것. proptypes 설치해야함. $yarn add prop-types하고 import.
+//         poster: PropTypes.string.isRequired, // 이걸 해주면 정보가 제대로 오는지, 내가 원하는 형식으로 오는 지 확인 가능.
+//     }
+//
+//     render() {
+//
+//         return(
+//
+//         )
+//     }
+// }
 
-    static propTypes = {
-        title: PropTypes.string.isRequired, // 부모에게서 string으로 받아야하고, 이 정보는 꼭 와야한다고 말해주는 것. proptypes 설치해야함. $yarn add prop-types하고 import.
-        poster: PropTypes.string.isRequired, // 이걸 해주면 정보가 제대로 오는지, 내가 원하는 형식으로 오는 지 확인 가능.
-    }
-
-    render() {
-        
-        return(
+function Movie({title, poster}) {
+    return (
         <div>
-            <MoviePoster poster={this.props.poster}/>
-        <h1>{this.props.title}</h1>
+        <MoviePoster poster={poster}/>
+        <h1>{title}</h1>
         </div>
-        )
-    }
+    )
+}
+Movie.propTypes = {
+    poster: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired
 }
 
-class MoviePoster extends Component{
-    render() {
-        return(
-            <img src={this.props.poster} />
-        )
-    }
+// class MoviePoster extends Component{
+//
+//     static propTypes = {
+//         poster: PropTypes.string.isRequired
+//     }
+//     render() {
+//         return(
+//             <img src={this.props.poster} />
+//         )
+//     }
+// }
+
+
+//위의 코드와 정확히 같은 것. 근데 아래 것은 state가 없는 dump component. 굳이 state가 필요없으면 밑에처럼
+// function형태로 만드는 게 낫다. 이렇게 만들면 앞에 App.js에서 썼던 여러 함수들 쓸 필요 없다는 뜻.
+// functional 컴포넌트는 state가 없다. 핵심.
+function MoviePoster({poster}){
+    return (
+        <img src={poster}  alt = "Movie Poster" />
+    )
+}
+
+// 그럼 functional 컴포넌트로 만들엇으면 propType은 어케 확인? 아래 처럼.
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
 }
 
 export default Movie
